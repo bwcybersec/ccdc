1550ad9b0a6475f7209fe4d11d4aecf1499dd66b
citty <8428080+famous1622@users.noreply.github.com>
2022-03-17 21:49:26 -0400

    thing

--- setup_vsftpd.sh
+++ setup_vsftpd.sh
@@ -2,4 +2,5 @@
 docker build . -t discord
 mkdir /PubDoc
 chown -R 14:14 /PubDoc
-docker run -d -v "/PubDoc":/var/ftp/PubDoc -p 20:20 -p 21:21 zdsftpd --restart=always --name vsftpd discord
+docker run -d -v "/PubDoc":/var/ftp/PubDoc -p 20:20 -p 21:21 --restart=always --name vsftpd discord
+
