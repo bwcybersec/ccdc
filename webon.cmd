b099c647de77a07dbc390d144932935ad98b9f01
katchins <katchins@bw.edu>
2023-02-19 22:17:55 -0500

    Update webon.cmd

--- webon.cmd
+++ webon.cmd
@@ -1,6 +1,4 @@
 @echo off
-Title "CCDC Windows Script"
-
 ::Generic Firewall rules
 netsh advfirewall firewall set rule name="CCDC-Web State" new dir=out action=allow enable=yes profile=any remoteip=any remoteport=80,443 protocol=tcp
 netsh advfirewall firewall set rule name="CCDC-DNS State" new dir=out action=allow enable=yes profile=any remoteport=53 protocol=udp
