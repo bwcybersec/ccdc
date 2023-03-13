d15f99f62f538fee8792ee0ecafdeb4649fa2f31
Baldwin Wallace CyberSec Team <77478140+bwcybersec@users.noreply.github.com>
2023-03-12 22:03:02 -0400

    Update webon.cmd

--- webon.cmd
+++ webon.cmd
@@ -1,4 +1,4 @@
 @echo off
 ::Generic Firewall rules
-netsh advfirewall firewall set rule name="CCDC-Web State" new dir=out action=allow enable=yes profile=any remoteip=any remoteport=80,443 protocol=tcp
-netsh advfirewall firewall set rule name="CCDC-DNS State" new dir=out action=allow enable=yes profile=any remoteport=53 protocol=udp
+netsh advfirewall firewall set rule name="CCDC-Web Regional" new dir=out action=allow enable=yes profile=any remoteip=any remoteport=80,443 protocol=tcp
+netsh advfirewall firewall set rule name="CCDC-DNS Regional" new dir=out action=allow enable=yes profile=any remoteport=53 protocol=udp
