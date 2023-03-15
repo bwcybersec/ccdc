e321bcb67fa14b9c957c406427d472f9ab9f57b6
Baldwin Wallace CyberSec Team <77478140+bwcybersec@users.noreply.github.com>
2023-03-14 23:18:59 -0400

    Update windown.ps1

--- windown.ps1
+++ windown.ps1
@@ -14,7 +14,8 @@ Invoke-WebRequest https://raw.githubusercontent.com/olafhartong/sysmon-modular/m
 Invoke-WebRequest https://winscp.net/download/WinSCP-5.21.7-Setup.exe -Outfile WinSCP.exe
 Invoke-WebRequest https://dl.google.com/chrome/install/375.126/chrome_installer.exe -Outfile chrome.exe
 Invoke-WebRequest https://download.sysinternals.com/files/ProcessMonitor.zip -Outfile procmon.zip
-Invoke-WebRequest shorturl.at/lwNS1 -Outfile firefox.exe
+# Get BlueSpawn after disabling Windows Defender
+#Invoke-WebRequest https://github.com/ION28/BLUESPAWN/releases/download/v0.5.1-alpha/BLUESPAWN-client-x64.exe -Outfile BLUESPAWN-client-x64.exe
 Invoke-WebRequest https://www.winpcap.org/install/bin/WinPcap_4_1_3.exe -Outfile WinPcap.exe
 #Invoke-WebRequest https://www.winpcap.org/windump/install/bin/windump_3_9_5/WinDump.exe -Outfile Windump.exe
 Invoke-WebRequest https://download.splunk.com/products/universalforwarder/releases/9.0.3/windows/splunkforwarder-9.0.3-dd0128b1f8cd-x64-release.msi -Outfile splunk.msi
