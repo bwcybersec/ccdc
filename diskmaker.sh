4475a78d3a81fd71d56fd6e8571a095fb4c8944d
XLH <124806900+xlhashbrown@users.noreply.github.com>
2024-02-07 20:01:07 -0500

    we aren't allowed to have nice things :(

--- diskmaker.sh
+++ diskmaker.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-dd if=/dev/zero of=vault.disk bs=8192 count=1000000 status=progress  # approx. 8GB
+dd if=/dev/zero of=vault.disk bs=8192 count=1000000  # approx. 8GB
 cryptsetup luksFormat --hash=sha256 --key-size=256 --cipher=aes-xts-plain64 --verify-passphrase vault.disk
 cryptsetup luksOpen vault.disk vault
+echo "Vault opened..."
 mkfs.ext4 -L VAULT /dev/mapper/vault
-mount -m /dev/mapper/vault /mnt/vault
+mkdir /mnt/vault
+mount /dev/mapper/vault /mnt/vault
 echo "Success!"
