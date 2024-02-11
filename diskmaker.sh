dc311778b95e06d59d06a8af3f2afcf06d93fc32
XLH <124806900+xlhashbrown@users.noreply.github.com>
2024-02-10 19:49:23 -0500

    Fedora is too old

--- diskmaker.sh
+++ diskmaker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dd if=/dev/zero of=vault.disk bs=8192 count=1000000  # approx. 8GB
-cryptsetup luksFormat --hash=sha256 --key-size=256 --cipher=aes-xts-plain64 --verify-passphrase vault.disk
+cryptsetup luksFormat --hash=sha256 --key-size=256 --cipher=serpent-xts-plain64 --verify-passphrase vault.disk
 cryptsetup luksOpen vault.disk vault
 echo "Vault opened..."
 mkfs.ext4 -L VAULT /dev/mapper/vault
