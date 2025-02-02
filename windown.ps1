9981c3ea21eb4cf6832892a68ecf5167a5ee17b8
katchins <katchins@bw.edu>
2025-02-01 19:51:40 -0500

    Update windown.ps1
    Caleb told me to do this...

--- windown.ps1
+++ windown.ps1
@@ -9,13 +9,11 @@ Invoke-WebRequest "https://raw.githubusercontent.com/bwcybersec/ccdc/main/weboff
 Write-Output "Download: webon"
 Invoke-WebRequest "https://raw.githubusercontent.com/bwcybersec/ccdc/main/webon.cmd" -Outfile webon.cmd
 
-Write-Output "Download: Autoruns.zip"
-Invoke-WebRequest https://download.sysinternals.com/files/Autoruns.zip -Outfile Autoruns.zip
-Expand-Archive .\Autoruns.zip c:\downloads -Force
+Write-Output "Download: Firefox.exe"
+Invoke-WebRequest "https://download.mozilla.org/?product=firefox-stub&os=win&lang=en-US&attribution_code=c291cmNlPXd3dy5nb29nbGUuY29tJm1lZGl1bT1yZWZlcnJhbCZjYW1wYWlnbj0obm90IHNldCkmY29udGVudD0obm90IHNldCkmZXhwZXJpbWVudD0obm90IHNldCkmdmFyaWF0aW9uPShub3Qgc2V0KSZ1YT1jaHJvbWUmY2xpZW50X2lkPShub3Qgc2V0KSZzZXNzaW9uX2lkPShub3Qgc2V0KSZkbHNvdXJjZT1tb3pvcmc.&attribution_sig=8050a714514346fdc6eb8a04a5cf8bad6805f8964fec63b63a8e91e7962fa0f7" -Outfile Firefox.exe
 
-Write-Output "Download: ProcessExplorer.zip"
-Invoke-WebRequest https://download.sysinternals.com/files/ProcessExplorer.zip -Outfile ProcessExplorer.zip
-Expand-Archive .\ProcessExplorer.zip c:\downloads -Force
+Write-Output "Download: Splunk"
+Invoke-WebRequest https://172.20.242.10:8000/splunkforwarder.msi -Outfile splunkforwarder.msi
 
 Write-Output "Download and Install: Sysmon.zip"
 Invoke-WebRequest https://download.sysinternals.com/files/Sysmon.zip -Outfile Sysmon.zip
@@ -34,7 +32,6 @@ Write-Output "Download: TCPView.zip"
 Invoke-WebRequest https://download.sysinternals.com/files/TCPView.zip -Outfile TCPView.zip
 Expand-Archive .\TCPView.zip c:\downloads -Force
 
-            # hard install
 Write-Output "Download: Putty.msi"
 Invoke-WebRequest https://the.earth.li/~sgtatham/putty/0.80/w64/putty-64bit-0.80-installer.msi -Outfile putty.msi
 
@@ -47,11 +44,13 @@ Invoke-WebRequest https://nmap.org/dist/nmap-7.94-setup.exe -Outfile nmap.exe
 Write-Output "Download: WinSCP.exe"
 #Invoke-WebRequest https://cdn.winscp.net/files/WinSCP-6.3.6-Setup.exe?secure=xtTFi18u2Hj7S-7PQ28VNg==,1737253242 -Outfile WinSCP.exe
 
-Write-Output "Download: Splunk"
-Invoke-WebRequest https://172.20.242.10:8000/splunkforwarder.msi -Outfile splunkforwarder.msi
+Write-Output "Download: Autoruns.zip"
+Invoke-WebRequest https://download.sysinternals.com/files/Autoruns.zip -Outfile Autoruns.zip
+Expand-Archive .\Autoruns.zip c:\downloads -Force
 
-Write-Output "Download: Firefox.exe"
-Invoke-WebRequest "https://download.mozilla.org/?product=firefox-stub&os=win&lang=en-US&attribution_code=c291cmNlPXd3dy5nb29nbGUuY29tJm1lZGl1bT1yZWZlcnJhbCZjYW1wYWlnbj0obm90IHNldCkmY29udGVudD0obm90IHNldCkmZXhwZXJpbWVudD0obm90IHNldCkmdmFyaWF0aW9uPShub3Qgc2V0KSZ1YT1jaHJvbWUmY2xpZW50X2lkPShub3Qgc2V0KSZzZXNzaW9uX2lkPShub3Qgc2V0KSZkbHNvdXJjZT1tb3pvcmc.&attribution_sig=8050a714514346fdc6eb8a04a5cf8bad6805f8964fec63b63a8e91e7962fa0f7" -Outfile Firefox.exe
+Write-Output "Download: ProcessExplorer.zip"
+Invoke-WebRequest https://download.sysinternals.com/files/ProcessExplorer.zip -Outfile ProcessExplorer.zip
+Expand-Archive .\ProcessExplorer.zip c:\downloads -Force
 
 # Get BlueSpawn after disabling Windows Defender
 #Invoke-WebRequest https://github.com/ION28/BLUESPAWN/releases/download/v0.5.1-alpha/BLUESPAWN-client-x64.exe -Outfile BLUESPAWN-client-x64.exe
