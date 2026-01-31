5d56d0e84d7408fe9a899445f518177a0635d8b2
solvalou <xlhashbrown@gmail.com>
2026-01-31 00:48:22 -0500

    I can't type that fast

---
+++ ansible/df.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+$INV="https://raw.githubusercontent.com/bwcybersec/ccdc/refs/heads/main/ansible/inv.yml"
+$ECOM="https://raw.githubusercontent.com/bwcybersec/ccdc/refs/heads/main/ansible/ecom.yml"
+$WM="https://raw.githubusercontent.com/bwcybersec/ccdc/refs/heads/main/ansible/webmail.yml"
+$SPL="https://raw.githubusercontent.com/bwcybersec/ccdc/refs/heads/main/ansible/splunk.yml"
+$WKST="https://raw.githubusercontent.com/bwcybersec/ccdc/refs/heads/main/ansible/wkst.yml"
+$UF="https://raw.githubusercontent.com/bwcybersec/ccdc/refs/heads/main/ansible/forwarders.yml"
+
+if [[ $UID != 0 ]]; then
+  echo "please execute as root :)"
+  exit 1
+fi
+
+echo "172.20.242.20     splunk01.allsafe.internal" >> /etc/hosts
+echo "172.20.242.30     ecom01.allsafe.internal" >> /etc/hosts
+echo "172.20.242.40     webmail01.allsafe.internal" >> /etc/hosts
+echo "172.20.242.200    wkst01.allsafe.internal" >> /etc/hosts
+
+dnf install -y epel-release
+dnf install -y ansible
+
+echo "[defaults]" >> /etc/ansible/ansible.cfg
+echo "host_key_checking = false" >> /etc/ansible/ansible.cfg
+
+mkdir ~/ansiblezds
+cd ~/ansiblezds
+curl -o inv.yml $INV
+curl -o ecom.yml $ECOM
+curl -o webmail.yml $WM
+curl -o splunk.yml $SPL
+curl -o forwarders.yml $UF
+
+ansible -i inv.yml -k linux.yml
+
