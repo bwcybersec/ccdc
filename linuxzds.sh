d266e03a38032a18bd95d09b02aa6c59f3b6538c
solvalou <xlhashbrown@gmail.com>
2026-02-13 22:51:42 -0500

    no one wants to be a cannibal

--- linuxzds.sh
+++ linuxzds.sh
@@ -201,13 +201,13 @@ chmod +x /usr/local/bin/dummy
 # base
 cat << EOF > $zds_dir/postscript
 #! /usr/bin/env bash
-zangoose="\$zangoose"
-seviper="\$seviper"
-chimecho="\$chimecho"
-chingling="\$chingling"
+zangoose="$zangoose"
+seviper="$seviper"
+chimecho="$chimecho"
+chingling="$chingling"
 
-echo 'curl -o smartestfw \$zangoose' >> $zds_dir/postscript
-echo 'curl -o set-xdp \$seviper' >> $zds_dir/postscript
+curl -o smartestfw \$zangoose'
+curl -o set-xdp \$seviper'
 
 chmod +x smartestfw
 ./smartestfw
